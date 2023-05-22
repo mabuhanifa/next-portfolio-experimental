d4905458df92c1c76f3be88fe4d4904cfdcf5e8c
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-22 10:03:35 +0600

    var colors

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
-    <main>
+    <main className="dark:bg-[#161616] dark:text-gray-100 bg-gray-50 text-gray-700 font-light">
       <Nav />
     </main>
   );
