4623b1d2b9af5238945c88671396a5c9df0163f8
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-22 14:27:33 +0600

    border

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main className="px-10 lg:px-20 dark:bg-[#161616] dark:text-gray-100 bg-gray-50 text-gray-700 font-light">
+    <main className="px-10 lg:px-20 dark:bg-[#161616] dark:text-gray-100 bg-gray-100 text-gray-700 font-light">
       <Nav />
       <Banner/>
       <Skills/>
